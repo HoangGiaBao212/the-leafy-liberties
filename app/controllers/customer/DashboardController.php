62f75919ca735d9e6b6c322a3a4ec66181e55ef8
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-09-18 19:20:13 +0700

    fix index dashboard controller

--- app/controllers/customer/DashboardController.php
+++ app/controllers/customer/DashboardController.php
@@ -6,6 +6,7 @@ use App\Models\Order;
 use App\Models\User;
 use Core\Application;
 use Core\Controller;
+use Core\Database;
 use Core\Request;
 use Core\Response;
 use Core\View;
@@ -31,7 +32,32 @@ class DashboardController extends Controller
     $pendingOrders = Order::findAll(["status" => "0"]);
     $customer = count(User::findAll(["deleted_at" => "null"]));
     $orders = count(Order::all());
-    
+    $db = Database::getInstance();
+    //top 5 product sold in one month
+    $top5prdSold = $db->select("SELECT p.id, p.name, SUM(op.quantity) AS total_quantity
+      FROM products p
+      JOIN orders_products op ON op.product_id = p.id
+      JOIN orders o ON o.id = op.order_id
+      WHERE o.status = 5
+      AND YEAR(o.create_at) = YEAR(CURRENT_DATE)
+      AND MONTH(o.create_at) = MONTH(CURRENT_DATE)
+      GROUP BY p.id, p.name
+      ORDER BY total_quantity DESC
+      LIMIT 5;", []);
+
+    $test = json_encode($top5prdSold);
+
+    // //most sold by category query
+    $categorySold = $db->select("SELECT c.id AS category_id, COUNT(DISTINCT o.id) AS num_orders
+      FROM categories c
+      JOIN products_categories pc ON pc.category_id = c.id
+      JOIN products p ON p.id = pc.product_id
+      JOIN orders_products op ON op.product_id = p.id
+      JOIN orders o ON o.id = op.order_id
+      WHERE o.status = 5
+      GROUP BY c.id
+      ORDER BY num_orders DESC
+      LIMIT 6;", []);
     $response->setStatusCode(200);
     $response->setBody(
       View::renderWithLayout(
@@ -42,7 +68,10 @@ class DashboardController extends Controller
           "successfulOrder" => $successfulOrder,
           "pendingOrders" => $pendingOrders,
           "customer" => $customer,
-          "orders" => $orders
+          "orders" => $orders,
+          "top5prdSold" => $top5prdSold,
+          "categorySold" => $categorySold,
+          "test" => $test,
         ],
         "layouts/dashboard"
       )
--- app/views/pages/dashboard/index.php
+++ app/views/pages/dashboard/index.php
@@ -2,36 +2,6 @@
 
 use App\Models\Category;
 use App\Models\Order;
-use App\Models\OrderProduct;
-use App\Models\User;
-use Core\Database;
-
-$db = Database::getInstance();
-//top 5 product sold in one month
-$top5prdSold = $db->select("SELECT p.id, p.name, SUM(op.quantity) AS total_quantity
-FROM products p
-JOIN orders_products op ON op.product_id = p.id
-JOIN orders o ON o.id = op.order_id
-WHERE o.status = 5
-AND YEAR(o.create_at) = YEAR(CURRENT_DATE)
-AND MONTH(o.create_at) = MONTH(CURRENT_DATE)
-GROUP BY p.id, p.name
-ORDER BY total_quantity DESC
-LIMIT 5;", []);
-
-$test = json_encode($top5prdSold);
-
-// //most sold by category query
-$categorySold = $db->select("SELECT c.id AS category_id, COUNT(DISTINCT o.id) AS num_orders
-FROM categories c
-JOIN products_categories pc ON pc.category_id = c.id
-JOIN products p ON p.id = pc.product_id
-JOIN orders_products op ON op.product_id = p.id
-JOIN orders o ON o.id = op.order_id
-WHERE o.status = 5
-GROUP BY c.id
-ORDER BY num_orders DESC
-LIMIT 6;", []);
 
 $sum = 0;
 $sumCategory = 0;
